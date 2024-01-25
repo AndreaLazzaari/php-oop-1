acdf907db5d1dd20807881c1c266b1f752d47335
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-25 19:11:54 +0100

    spostato lavoro in index.php

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+
 <?php
 
-?>
+/**
+ * generete a single movie
+ */
+class Movie{
+    public $genere;
+    public $titolo;
+    public $dataUscita;
+    public $regista;
+
+    /**
+     * Construct new movie
+     *
+     * @param string $_genere what type a movie
+     * @param string $_titolo film titol
+     */
+    public function __construct(string $_genere, string $_titolo) {
+        $this->genere = $_genere;
+        $this->titolo = $_titolo;
+    }
+
+    public function getLenghtString() {
+        return strlen($this->titolo);
+    }
+}
+
+$shrekMovie = new Movie('Commedy, Cartoon', 'Shrek');
+$cenerentolaMovie = new Movie('Cartoon', 'Cenerentola');
+
+
+
+var_dump($shrekMovie, $cenerentolaMovie);
+var_dump($shrekMovie->getLenghtString(), $cenerentolaMovie->getLenghtString());

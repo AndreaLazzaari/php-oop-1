f4a79aa4d6dd0d0e5744b473527396c939208b1a
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-25 19:11:14 +0100

    commentato generazione di class e costruttori

--- Classes/Movie.php
+++ Classes/Movie.php
@@ -1,13 +1,22 @@
 
 <?php
 
+/**
+ * generete a single movie
+ */
 class Movie{
     public $genere;
     public $titolo;
     public $dataUscita;
     public $regista;
 
-    public function __construct($_genere, $_titolo) {
+    /**
+     * Construct new movie
+     *
+     * @param string $_genere what type a movie
+     * @param string $_titolo film titol
+     */
+    public function __construct(string $_genere, string $_titolo) {
         $this->genere = $_genere;
         $this->titolo = $_titolo;
     }
@@ -18,9 +27,9 @@ class Movie{
 }
 
 $shrekMovie = new Movie('Commedy, Cartoon', 'Shrek');
+$cenerentolaMovie = new Movie('Cartoon', 'Cenerentola');
 
 
 
-
-var_dump($shrekMovie);
-var_dump($shrekMovie->getLenghtString());
+var_dump($shrekMovie, $cenerentolaMovie);
+var_dump($shrekMovie->getLenghtString(), $cenerentolaMovie->getLenghtString());

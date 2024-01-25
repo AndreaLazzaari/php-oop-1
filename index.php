51c394757d3b2d311f28d2f83e3dcb23415b3d2d
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-25 19:24:58 +0100

    aggiunto boostrap e aggiunta una card

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-
-<?php
-
+<?php 
 /**
  * generete a single movie
  */
@@ -30,6 +28,38 @@ $shrekMovie = new Movie('Commedy, Cartoon', 'Shrek');
 $cenerentolaMovie = new Movie('Cartoon', 'Cenerentola');
 
 
+?>
 
-var_dump($shrekMovie, $cenerentolaMovie);
-var_dump($shrekMovie->getLenghtString(), $cenerentolaMovie->getLenghtString());
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="card col-4" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $shrekMovie->titolo?></h5>
+                        <p class="card-text"><?php echo $shrekMovie->genere?></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </main>
+</body>
+
+</html>

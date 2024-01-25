dd1a310406f0c5001838cd891046644915b56a69
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-25 19:31:33 +0100

    create due card con all'interno dati dei due film

--- index.php
+++ index.php
@@ -5,18 +5,19 @@
 class Movie{
     public $genere;
     public $titolo;
-    public $dataUscita;
-    public $regista;
+    public $locandina;
 
     /**
      * Construct new movie
      *
      * @param string $_genere what type a movie
      * @param string $_titolo film titol
+     * @param string $_locandina locandina opzionale del film
      */
-    public function __construct(string $_genere, string $_titolo) {
+    public function __construct(string $_genere, string $_titolo, string $_locandina = null) {
         $this->genere = $_genere;
         $this->titolo = $_titolo;
+        $this->locandina = $_locandina;
     }
 
     public function getLenghtString() {
@@ -24,8 +25,8 @@ class Movie{
     }
 }
 
-$shrekMovie = new Movie('Commedy, Cartoon', 'Shrek');
-$cenerentolaMovie = new Movie('Cartoon', 'Cenerentola');
+$shrekMovie = new Movie('Commedy, Cartoon', 'Shrek', 'https://c8.alamy.com/compit/2jdpenf/poster-del-film-shrek-2001-2jdpenf.jpg');
+$cenerentolaMovie = new Movie('Cartoon', 'Cenerentola', 'https://pad.mymovies.it/filmclub/2002/06/027/imm.jpg');
 
 
 ?>
@@ -47,10 +48,18 @@ $cenerentolaMovie = new Movie('Cartoon', 'Cenerentola');
         <div class="container">
             <div class="row">
                 <div class="card col-4" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="<?php echo $shrekMovie->locandina?>" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $shrekMovie->titolo?></h5>
                         <p class="card-text"><?php echo $shrekMovie->genere?></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card col-4" style="width: 18rem;">
+                    <img src="<?php echo $cenerentolaMovie->locandina?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $cenerentolaMovie->titolo?></h5>
+                        <p class="card-text"><?php echo $cenerentolaMovie->genere?></p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
